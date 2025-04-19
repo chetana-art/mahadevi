2423595aebc56f6285ab3fbd67f3cbcb847590d1
chetana-art <chetana.sadanand.chavan@gmail.com>
2025-04-19 12:08:48 +0530

    Add files via upload

---
+++ pg2.py
@@ -0,0 +1,7 @@
+a=20
+b=10
+sum=a+b
+sub=a-b
+print('the value of a is{} and b is{}'.format(a,b))
+print('{2} is the sum of {0} and {1}'.format(a,b,sum))
+print('{sub_value} is the subtraction of {value_a} and {value_b}'.format(value_a=a,value_b=b,sub_value=sub))
